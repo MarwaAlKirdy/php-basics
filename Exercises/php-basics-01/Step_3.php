9811afccb3601b022d00dd19758623a25395635e
Marwa Al Kirdy <marwaalkirdy96@gmail.com>
2020-10-06 09:58:23 +0300

    Completed php-basics-01 Step_3

---
+++ Exercises/php-basics-01/Step_3.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Marwa";
+$email = "marwaalkirdy96@gmail.com";
+echo "My name is " . $name . " and my email is " . $email . "<br>";
+$height = 5;
+$width = 5;
+$area = $height * $width;
+echo "The area is: " . $area;
