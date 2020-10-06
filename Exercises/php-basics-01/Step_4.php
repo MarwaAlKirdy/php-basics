211781c93e280c18b198f42124302ce796b55692
Marwa Al Kirdy <marwaalkirdy96@gmail.com>
2020-10-06 10:34:01 +0300

    Completed php-basics-01 Step_4

---
+++ Exercises/php-basics-01/Step_4.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Marwa";
+$position = "Codi student";
+$github_url = "https://github.com/MarwaAlKirdy";
+
+echo "Hello, I'm " . $name . " I'm a " . $position . " please check my github link " . $github_url;
+echo"<br>";
+echo "Hello, I'm {$name} I'm a {$position} please check my github link {$github_url}";
+echo "<br>";
+echo 'Hello, I\'m ' . $name . ' I\'m a ' . $position . ' please check my github link ' . $github_url;
+echo "<br>";
+echo "Hello, I'm  "," $name I'm a "," $position please check my github link "," $github_url";
+
+?>
